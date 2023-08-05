faa1c40897f49b293ccbb48bbac2f91cbd4b11b1
PrinceRupapara <141495531+PrinceRupapara@users.noreply.github.com>
2023-08-05 17:31:33 +0530

    Add files via upload

---
+++ Area of tringle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c=0.5,area;
+	printf("enter two values");
+	scanf("%d%d",&a,&b);
+	area=a*b*c;
+	printf("area=%d",area);
+	return 0;
+}
---
+++ F to c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int f,c;
+	printf("enter a value");
+	scanf("%d",&f);
+	c=5*f-32/9;
+	printf("c=%d",c);
+	return 0;
+}
